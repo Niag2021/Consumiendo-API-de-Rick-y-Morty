bd9bbbf0c002c45c1ccfa61ceb4113bc28d2caa5
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-27 16:46:03 -0300

    Listo.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getCharacters(done){
+    const results = fetch ("https://rickandmortyapi.com/api/character");
+    
+    results
+        .then(response => response.json())
+        .then(data => (
+            done(data)
+        ));
+}
+
+getCharacters(data=>{
+    //console.log(data);
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(`
+            <article>
+                <div class="image-container">
+                   <img src="${personaje.image}" alt="Personaje">
+                </div>
+                <h2>${personaje.name}</h2>
+                <span>${personaje.status}</span>
+            </article>        
+        `);
+        //Mostras en pantalla.
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+});
+
